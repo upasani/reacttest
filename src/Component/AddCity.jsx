17ee99051e337279c60ddb20320a7eb2ba517f16
Vaibhav Upasani <upasanivaibhav7@gmail.com>
2022-04-16 19:49:31 +0530

    added code

--- src/Component/AddCity.jsx
+++ src/Component/AddCity.jsx
@@ -1,5 +1,25 @@
+import { useState } from "react"
+
 export const AddCity=()=>{
+    const [city,update]=useState("");
+    const [population,updatepop]=useState();
+    const [country,updatecoun]=useState("");
+    const addcity=()=>{
+        console.log(city ,"city is");
+        fetch(``)
+
+    }
     return(
-        <div>This is add city</div>
+        <div>
+            <input type="text" required onChange={(e)=>update(e.target.value)}  placeholder="Enter the city name"/>
+            <input type="number"
+            onChange={(e)=>updatepop(e.target.value)}
+             required placeholder="Enter the population" />
+            <input type="text" 
+             onChange={(e)=>updatepop(e.target.value)}
+         
+            required placeholder="Enter the country name" />
+            <button onClick={addcity}>ADD CTIY</button>
+        </div>
     )
 }
